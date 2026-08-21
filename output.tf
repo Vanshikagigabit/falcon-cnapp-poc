45071e436e82369445cca148a5c045ba5f47f406
Vanshika Sharma <vanshikas@gigabittech.in>
2026-08-21 15:46:22 +0530

    Test Falcon IaC S3 misconfiguration

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
+  description = "my-bucket-misconfigured "
   value       = aws_s3_bucket.falcon_poc_bucket.bucket
 }
 
 output "s3_bucket_arn" {
-  description = "ARN of the S3 bucket"
+  description = "arn:aws:s3:::my-bucket-misconfigured"
   value       = aws_s3_bucket.falcon_poc_bucket.arn
 }
--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region where the POC resources will be created"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "bucket_name" {
-  description = "Name of the Falcon CNAPP POC S3 bucket"
+  description = "my-bucket-misconfigured "
   type        = string
   default     = "falcon-cnapp-poc-test-bucket"
 }
